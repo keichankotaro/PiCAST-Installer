a894c461756d8593493b0f86770d1602e1fe3305
keisukekisu <44016629+keisukekisu@users.noreply.github.com>
2020-05-16 13:40:32 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+cd ~
+echo "install git and npm."
+sudo apt -y install git npm
+echo "ok"
+curl -OL http://raw.github.com/lanceseidman/PiCAST/master/setup.sh
+sudo chmod +x setup.sh
+sudo cp -R /root/PiCAST ~/
+sudo chown -R pi:pi /home/pi/PiCAST
+sudo npm install -g express
+export NODE_PATH=/usr/local/lib/node_modules
+cd /home/pi/PiCAST
+./picast_stop.sh
+./picast_start.sh
+echo "Done!"
