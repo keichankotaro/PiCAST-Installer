100dcb8de455880c9c086819637c68abe99064ae
keichankotaro <44016629+keisukekisu@users.noreply.github.com>
2020-09-19 19:21:17 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ sudo apt -y install git npm
 echo "ok"
 curl -OL http://raw.github.com/lanceseidman/PiCAST/master/setup.sh
 sudo chmod +x setup.sh
+./setup.sh
 sudo cp -R /root/PiCAST ~/
 sudo chown -R pi:pi /home/pi/PiCAST
 sudo npm install -g express
